a265a11cae8056ac94dc17680fe1f8fbdd793390
Pure Insight <mitrantiennam@gmail.com>
2023-05-10 21:22:23 +0700

    Update Topic Multiple Browser and WebDriverManager

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,35 @@
 package commons;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
+	private WebDriver driver;
+	private String projectPath = System.getProperty("user.dir");
+
+	protected WebDriver getBrowserDriver(String browserName) {
+		switch (browserName.toLowerCase()) {
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			break;
+		case "chrome":
+			driver = WebDriverManager.chromedriver().create();
+			break;
+		case "edge":
+			driver = WebDriverManager.edgedriver().create();
+			break;
+		default:
+			throw new RuntimeException("Browser name invalid");
+		}
+		driver.manage().window().maximize();
+		driver.get("https://demo.nopcommerce.com/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		return driver;
+	}
 
 }
---
+++ testcases/com/nopcommerce/user/Level_04_Multiple_Browser.java
@@ -0,0 +1,82 @@
+package com.nopcommerce.user;
+
+import java.util.Random;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.BaseTest;
+import pageObjects.HomePageObject;
+import pageObjects.RegisterPageObject;
+
+public class Level_04_Multiple_Browser extends BaseTest {
+	private WebDriver driver;
+	private String firstName, lastName, invalidEmailAddress, password, confirmPassword;
+	private HomePageObject homePage;
+	private RegisterPageObject registerPage;
+
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+		driver = getBrowserDriver(browserName);
+		homePage = new HomePageObject(driver);
+
+		firstName = "Pure";
+		lastName = "Insight";
+		invalidEmailAddress = "123321";
+		password = confirmPassword = "pureinsight";
+	}
+
+	@Test
+	public void Register_01_Register_Empty_Data() {
+		System.out.println("Register_01 - Step 01: Click to Register link");
+		homePage.clickToRegisterLink();
+		registerPage = new RegisterPageObject(driver);
+
+		System.out.println("Register_01 - Step 02: Click to Register button");
+		registerPage.clickToRegisterButton();
+
+		System.out.println("Register_01 - Step 03: Verify error message is displayed");
+		Assert.assertEquals(registerPage.getErrorMessageAtFirstNameTextbox(), "First name is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtLastNameTextbox(), "Last name is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtEmailTextbox(), "Email is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtPasswordTextbox(), "Password is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtConfirmPasswordTextbox(), "Password is required.");
+	}
+
+	@Test
+	public void Register_02_Register_Invalid_Email() {
+		System.out.println("Register_02 - Step 01: Click to Register link");
+		homePage.clickToRegisterLink();
+		registerPage = new RegisterPageObject(driver);
+
+		System.out.println("Register_02 - Step 02: input to required fields ");
+		registerPage.inputToFirstNameTextbox(firstName);
+		registerPage.inputToLastNameTextbox(lastName);
+		registerPage.inputToEmailTextbox(invalidEmailAddress);
+		registerPage.inputToPasswordTextbox(password);
+		registerPage.inputToConfirmPasswordTextbox(confirmPassword);
+
+		System.out.println("Register_02 - Step 03: Click to Register button");
+		registerPage.clickToRegisterButton();
+
+		System.out.println("Register_02 - Step 04: Verify error message at email textbox");
+		Assert.assertEquals(registerPage.getErrorMessageAtEmailTextbox(), "Wrong email");
+
+	}
+
+	public int getRandomNumber() {
+		Random random = new Random();
+		return random.nextInt(9999);
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
